5123b3b6dcbab7080f3059dbe9b28985978a77e6
Danial <fzdanial09@gmail.com>
2023-06-11 18:54:06 +0800

    Updated Project & Skills

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           <Routes>
             <Route path="/" element={<About />} />
             <Route path="/team" element={<Team />} />
-            <Route path="/invoices" element={<Invoices />} />
+            {/* <Route path="/invoices" element={<Invoices />} /> */}
             <Route path="/contacts" element={<Contacts />} />
             {/* <Route path="/bar" element={<Bar />} /> */}
             {/* <Route path="/form" element={<Form />} /> */}
