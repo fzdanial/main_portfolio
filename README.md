914712137da7e9fa234fbf0d89d26f13645514c0
Danial <fzdanial09@gmail.com>
2023-06-22 02:11:54 +0800

    updated projects page and readme

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ This is my portfolio website.
 
 This site is meant to showcase all my projects and learnings so far. 
 This site will be hosted with the following link:
-/* ADD LINK HERE */
+https://fzdanial.onrender.com/
 
 Thank you for visiting!
--- src/scenes/project/index.jsx
+++ src/scenes/project/index.jsx
@@ -10,6 +10,21 @@ const Projects = () => {
     return (
         <Box m="20px">
             <Header title="Previous Projects"  subtitle="Notable personal projects"/>
+            <Typography
+                variant="h5"
+                color = {colors.grey[300]}
+                sx = {{m: "15px 0 5px 20px"}}
+            >
+                This page is currently a work in progress. For more information on my previous work, please head over to my links at the top left corner.
+                You may also check out my GitHub page 
+                <a 
+                    href="https://github.com/fzdanial"
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                > 
+                        here
+                    </a>. 
+            </Typography>
         </Box>
     );
 };
